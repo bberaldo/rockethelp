2e6781979b218b70700aa877ead95eb4e5c0f1b8
Beatriz <biaberaldo@outlook.com>
2022-07-23 18:36:36 -0300

    update

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import auth from '@react-native-firebase/auth';
+import { Alert } from 'react-native';
 import { VStack, Heading, Icon, useTheme } from 'native-base';
 import { Envelope, Key } from 'phosphor-react-native';
 
@@ -7,12 +9,14 @@ import { Input } from '../components/Input';
 import { Button } from '../components/Button';
 
 export function SignIn() {
-	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const { colors } = useTheme();
 
 	function handleSignIn() {
-		console.log(name, password);
+		if (!email || !password) {
+			return Alert.alert('Entrar', 'Informe e-mail e senha');
+		}
 	}
 
 	return (
@@ -29,7 +33,7 @@ export function SignIn() {
 				InputLeftElement={
 					<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
 				}
-				onChangeText={setName}
+				onChangeText={setEmail}
 			/>
 
 			<Input
